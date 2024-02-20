dba299dc77c036c7216c753e8bd28cb0c813f159
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2024-02-19 23:12:32 -0300

    enviando array de tasks ao invés de apenas um

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,33 @@ import { ListaTasks } from './components/lista-tasks';
 import ButtonsLaterais from './components/buttons-laterais';
 import Task from './components/task';
 import './styles/style.css';
-interface  TaskProps{
-      taskmessage: string;
-      dataAtual: string;
-      tipoTask: string;
- 
+interface TaskProps {
+  taskmessage: string;
+  dataAtual: string;
+  tipoTask: string;
+
 }
 
 function App() {
 
   const [taskMessage, setTaskMessage] = useState({} as TaskProps);
-
+  let arrayTask:TaskProps[] =[];
+  
   const handleEnviarTask = (mensagem: any) => {
-    setTaskMessage(mensagem); // Atualiza o estado com a mensagem recebida do input
+    setTaskMessage(mensagem);
+     // Atualiza o estado com a mensagem recebida do input
+     arrayTask = [...arrayTask,taskMessage ];
   };
-  console.log(taskMessage)
+  console.log(arrayTask.map(a => console.log(a.taskmessage)))
+
+  
   return (
     <div className="App">
       <EntradaDados onEnviar={handleEnviarTask} />
       <ListaTasks>
-        <Task childContent = {taskMessage} />
+        {
+          arrayTask.length > 1 ? <Task childContent={arrayTask} /> : null
+        }
       </ListaTasks>
       <ButtonsLaterais></ButtonsLaterais>
     </div>
--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,25 +1,40 @@
 import '../styles/style.css'
 import { CiCalendarDate } from "react-icons/ci";
 
-interface  TaskProps{
-    childContent:{
-        taskmessage: string;
-        dataAtual: string;
-        tipoTask: string;
-    }
-   
+interface TaskProps {
+    childContent: das[];
+}
+interface das {
+    taskmessage?: string,
+    dataAtual?: string,
+    tipoTask?: string
+
 }
 
-export default function Task({ childContent }: TaskProps ) {
-
-    const { taskmessage, dataAtual, tipoTask } = childContent;
+const task: React.FC<TaskProps> = ({ childContent }) => {
     return (
-        <div className='task-card'>
-           <p><CiCalendarDate/>{dataAtual}</p>
-           <p>{taskmessage}</p>
-           <p className='tipo-Task'>{tipoTask}</p>
-           
+        <div>
+            {childContent.map((f, index) => (
+                <div className='task-card' key={index}>
+                    <p><CiCalendarDate />{f.dataAtual}</p>
+                    <p>{f.taskmessage}</p>
+                    <p className='tipo-Task'>{f.tipoTask}</p>
+                </div>
+            ))}
         </div>
     )
 }
+export default task;
+/*export default function Task({ childContent }: TaskProps) {
+    return (
+       childContent.map(f => {
+        <div className='task-card'>
+        <p><CiCalendarDate />{f.dataAtual}</p>
+        <p>{f.taskmessage}</p>
+        <p className='tipo-Task'>{f.tipoTask}</p>
+
+    </div>
+       })
+    )
+}*/
 
